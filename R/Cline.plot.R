9925679717515eb7e66df66c6d0b5e9a92ba8589
Ben Fitzpatrick <benfitz@utk.edu>
2012-05-09 00:00:00 +0000

    version 2.0

--- R/Cline.plot.R
+++ R/Cline.plot.R
@@ -1,48 +1,115 @@
 Cline.plot <-
-function(cfit,...){
-	Bart.fn <- function(x,a,b){x+2*x*(1-x)*(a+b*(2*x-1))}
-	Rich.fn <- function(x,U,L,b,m){U+(L-U)/(1+exp(b*(x-m)))}
-	if(length(cfit$Models)==1){par(mar=c(3,4,2,1),tcl=-.25,mgp=c(2,1,0),cex=1.25,lwd=2)}
-	if(length(cfit$Models)==2){par(mfrow=c(1,2),mar=c(3,4,2,1),tcl=-.25,mgp=c(2,1,0),cex=1.25,lwd=2)}
-	if(length(cfit$Models)==3){par(mfrow=c(1,3),mar=c(3,4,2,1),tcl=-.25,mgp=c(2,1,0),cex=1.25,lwd=2)}
-	if(length(cfit$Models)==4){par(mfrow=c(2,2),mar=c(3,4,2,1),tcl=-.25,mgp=c(2,1,0),cex=1.25,lwd=2)}
-	if(length(cfit$Models)==5){par(mfrow=c(2,3),mar=c(3,4,2,1),tcl=-.25,mgp=c(2,1,0),cex=1.25,lwd=2)}
-	X <- 0:100/100
-	L <- dim(cfit$Comparison)[1]
-	if(!is.na(match("Barton",cfit$Models))){
-	plot(0:1,0:1,xlab=expression(italic(S)),ylab=expression(italic(s[i])),type="l",lty=2,main="Barton clines")
-	for(i in 1:L){
-		lines(X,X+2*X*(1-X)*(cfit$Barton[i,1]+cfit$Barton[i,2]*(2*X-1)),lwd=2,col="grey")
+function(cfit){
+	llogit.p <-function(x,coef){
+		u <- as.numeric(coef)[1]
+		v <- as.numeric(coef)[2]
+		x^v/(x^v + (1-x)^v*exp(u))
+		}
+	Barton.p <- function(x,coef){
+		coef <- as.numeric(coef)
+		x+2*x*(1-x)*(coef[1]+coef[2]*(2*x-1))
+		}
+	Beta.p <- function(x,coef){
+		coef <- as.numeric(coef)
+		pbeta(x,coef[1]*coef[2],(1-coef[1])*coef[2])
+		}
+	L.p <- function(x,coef){
+		coef <- as.numeric(coef)
+		exp(coef[1]+coef[2]*x)/(1+exp(coef[1]+coef[2]*x))
+		}
+	Mn.p <- function(x,coef){
+		coef <- as.numeric(coef)
+		Hom <- exp(coef[3]+x*coef[4])/(1+exp(coef[1]+x*coef[2])+exp(coef[3]+x*coef[4]))
+		Het <- exp(coef[1]+x*coef[2])/(1+exp(coef[1]+x*coef[2])+exp(coef[3]+x*coef[4]))
+		data.frame(Hom,Het)
+		}
+	Rich.p <- function(x,coef){
+		coef <- as.numeric(coef)
+		U <- coef[1];L <- coef[2]; b <- coef[3]; m <- coef[4]
+		U+(L-U)/(1+exp(b*(x-m)))
+		}
+	nmod <- 0
+	Models <- character()
+	if(!is.null(cfit$Barton)){nmod <- nmod+1; Models[1] <- "Barton"}
+	if(!is.null(cfit$beta)){nmod <- nmod+1; Models[2] <- "beta"}
+	if(!is.null(cfit$logit.logistic)){nmod <- nmod+1; Models[3] <- "logit-logistic"}
+	if(!is.null(cfit$binom)){nmod <- nmod+1; Models[4] <- "binomial"}
+	if(!is.null(cfit$multinom)){nmod <- nmod+1; Models[5] <- "multinomial"}
+	if(!is.null(cfit$Richards)){nmod <- nmod+1; Models[6] <- "Richards"}
+	
+	par(mfrow=c(nmod,3),mar=c(2,2,.5,.5),mgp=c(1,.25,0),tcl=-.2)
+	
+	Sx <- seq(from=0,to=1,length.out=200)
+	cols <- c("black","red")
+	
+	if(!is.null(cfit$Barton)){
+		plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," Barton")))
+		for(i in 1:dim(cfit$Barton)[1]){
+			lines(Sx,Barton.p(Sx,cfit$Barton[i,1:2]),col=cols[1+1*cfit$Barton[i,7]])
+			}
+		plot(cfit$Barton[,1:2],xlab=expression(paste(italic(a))),ylab=expression(paste(italic(b))),col=cols[1+1*cfit$Barton[,7]])
+		o <- order(cfit$Barton[,6])
+		XQ <- qchisq(rank(cfit$Barton[,6])/(1+length(cfit$Barton[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$Barton[,5],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$Barton[,7]])
+	lines(XQ[o],XQ[o])
+		}
+	if(!is.null(cfit$beta)){
+			plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," beta")))
+		for(i in 1:dim(cfit$beta)[1]){
+			lines(Sx,Beta.p(Sx,cfit$beta[i,1:2]),col=cols[1+1*cfit$beta[i,7]])
+			}
+		plot(cfit$beta[,1:2],xlab=expression(paste(italic(mu))),ylab=expression(paste(italic(nu))),col=cols[1+1*cfit$beta[,7]])
+		o <- order(cfit$beta[,6])
+		XQ <- qchisq(rank(cfit$beta[,6])/(1+length(cfit$beta[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$beta[,5],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$beta[,7]])
+	lines(XQ[o],XQ[o])
+}
+	if(!is.null(cfit$logit.logistic)){
+		plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," logit-logistic")))
+		for(i in 1:dim(cfit$logit)[1]){
+			lines(Sx,llogit.p(Sx,cfit$logit[i,1:2]),col=cols[1+1*cfit$logit[i,7]])
+			}
+		plot(cfit$logit[,1:2],xlab=expression(paste(italic(u))),ylab=expression(paste(italic(v))),col=cols[1+1*cfit$logit[,7]])
+		o <- order(cfit$logit[,6])
+		XQ <- qchisq(rank(cfit$logit[,6])/(1+length(cfit$logit[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$logit[,5],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$logit[,7]])
+	lines(XQ[o],XQ[o])
 	}
-	lines(0:1,0:1,lty=2,lwd=2)
+	if(!is.null(cfit$binom)){
+		plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," binomial")))
+		for(i in 1:dim(cfit$binom)[1]){
+			lines(Sx,L.p(Sx,cfit$binom[i,1:2]),col=cols[1+1*cfit$binom[i,7]])
+			}
+		plot(cfit$binom[,1:2],xlab=expression(paste(italic(alpha))),ylab=expression(paste(italic(beta))),col=cols[1+1*cfit$binom[,7]])
+		o <- order(cfit$binom[,6])
+		XQ <- qchisq(rank(cfit$binom[,6])/(1+length(cfit$binom[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$binom[,5],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$binom[,7]])
+	lines(XQ[o],XQ[o])
 	}
-	if(!is.na(match("Beta",cfit$Models))){
-	plot(0:1,0:1,xlab=expression(italic(S)),ylab=expression(italic(s[i])),type="l",lty=2,main="Beta clines")
-	for(i in 1:L){
-		lines(X,pbeta(X,cfit$Beta[i,1]*cfit$Beta[i,2],(1-cfit$Beta[i,1])*cfit$Beta[i,2]),lwd=2,col="grey")
-	}
-	lines(0:1,0:1,lty=2,lwd=2)
-	}
-	if(!is.na(match("Richards",cfit$Models))){
-	plot(0:1,0:1,xlab=expression(italic(S)),ylab=expression(italic(s[i])),type="l",lty=2,main="Richards clines")
-	for(i in 1:L){
-		lines(X,cfit$Rich[i,1]+(cfit$Rich[i,2]-cfit$Rich[i,1])/(1+exp(cfit$Rich[i,3]*(X-cfit$Rich[i,4]))),lwd=2,col="grey")
-	}
-	lines(0:1,0:1,lty=2,lwd=2)
-	}
-	if(!is.na(match("heterozygosity",cfit$Models))){
-	plot(c(0,.5,1,0),c(0,1,0,0),xlab=expression(italic(S)),ylab=expression(italic(h[i])),type="l",lty=3,main=c("Heterozygosity","(symmetrical)"))
-	for(i in 1:L){
-		lines(X,(cfit$Het[i,1]*2*X*(1-X))^cfit$Het[i,2],lwd=2,col="grey")
-	}
-	lines(X,2*X*(1-X),lty=2,lwd=2)	
-	}
-	if(!is.na(match("Hbeta",cfit$Models))){
-	plot(c(0,.5,1,0),c(0,1,0,0),xlab=expression(italic(S)),ylab=expression(italic(h[i])),type="l",lty=3,main=c("Heterozygosity","(asymmetrical)"))
-	for(i in 1:L){
-		lines(X,cfit$Hb[i,1]*2*X^cfit$Hb[i,2]*(1-X)^cfit$Hb[i,3],lwd=2,col="grey")
-	}
-	lines(X,2*X*(1-X),lty=2,lwd=2)	
-	barplot(table(cfit$Comp[,2]>0,cfit$Comp[,6]))
+	if(!is.null(cfit$Richards)){
+		plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," Richards")))
+		for(i in 1:dim(cfit$Richards)[1]){
+			lines(Sx,Rich.p(Sx,cfit$Richards[i,1:4]),col=cols[1+1*cfit$Richards[i,9]])
+			}
+		plot(1,1,type="n",bty="n",xlab="",ylab="",xaxt="n",yaxt="n")
+		o <- order(cfit$Richards[,8])
+		XQ <- qchisq(rank(cfit$Richards[,8])/(1+length(cfit$Richards[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$Richards[,7],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$Richards[,9]])
+	lines(XQ[o],XQ[o])
+}
+	if(!is.null(cfit$multinom)){
+		plot(0:1,0:1,type="n",xlab=expression(paste(italic(S))),ylab=expression(paste(italic(p[i])," multinomial")))
+		for(i in 1:dim(cfit$multinom)[1]){
+			lines(Sx,Mn.p(Sx,cfit$multinom[i,1:4])$Hom,col=cols[1+1*cfit$multinom[i,9]])
+			}
+		for(i in 1:dim(cfit$multinom)[1]){
+			lines(Sx,Mn.p(Sx,cfit$multinom[i,1:4])$Het,col="grey")
+			if(cfit$multinom[i,9]){lines(Sx,Mn.p(Sx,cfit$multinom[i,1:4])$Het,col="red",lty=2)}
+			}
+		plot(1,1,type="n",bty="n",xlab="",ylab="",xaxt="n",yaxt="n")
+		o <- order(cfit$multinom[,8])
+		XQ <- qchisq(rank(cfit$multinom[,8])/(1+length(cfit$multinom[,6])),df=2,lower.tail=FALSE)
+		plot(XQ,cfit$multinom[,7],xlab=expression(paste(chi^2," quantiles")),ylab=expression(paste(italic(D)^2)),col=cols[1+1*cfit$multinom[,9]])
+	lines(XQ[o],XQ[o])
 	}
 }
--- R/gcline.fn.R
+++ R/gcline.fn.R
@@ -1,5 +1,9 @@
-gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,SD=rep(0.01,length(start)),headstart=FALSE,Grid=TRUE){
-	start <- as.numeric(start)
+gcline.fn <- function(x,n,y,start=NULL,model="logit-logit",method="L-BFGS-B",iterations=99,SD=rep(0.01,length(start)),headstart=FALSE,Grid=TRUE){
+	# requires nnet and stats
+	require(stats)
+	require(nnet)
+	# this function is for fitting models, not identifying outliers
+	if(!is.null(start)) start <- as.numeric(start)
 	X.out <- is.na(y)
 	if(sum(X.out)>0){
 		X.in <- !is.na(y)
@@ -8,10 +12,53 @@ gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,S
 		y <- y[X.in]
 		cat("\n",sum(X.out)," NA's were omitted\n")
 		}
-	if(model=="Beta"){
+	if(model=="logistic"){
+		method <- "glm.fit"
+		Y <- cbind(y,n-y)
+		fit.glm <- glm(Y~x,family="binomial")
+		LL.fit <- logLik(fit.glm)
+		k <- attr(LL.fit,"df")
+		est <- list(par=fit.glm$coef,convergence=fit.glm$conv,value=LL.fit[1])
+		null.LL <- NA
+	}
+	if(model=="multinom"){
+		if(!is.matrix(y)){stop("error, multinomial regression requires a matrix of genotype counts")}
+		method <- "nnet"
+		fit.mlt <- multinom(y~x,trace=FALSE)
+		LL.fit <- logLik(fit.mlt)
+		k <- attr(LL.fit,"df")
+		est <- list(par=summary(fit.mlt)$coeff,convergence=fit.mlt$conv,value=LL.fit[1])
+		null.LL <- NA
+	}
+	if(model=="logit-logit"){
+		if(is.null(start)){ start <- c(u=0,v=1)}
 		k <- 2
-		LL.fn <- function(par,x,n,y){
-			p <- pbeta(x,par[1]*par[2],(1-par[1])*par[2])
+		SD <- rep(0.1,2)
+		LL.fn <- function(par,SD=SD,x,n,y){
+			u <- par[1]
+			v <- par[2]
+			p <- x^v/(x^v + (1-x)^v*exp(u))
+			p <- replace(p,x==1,1)
+			p <- replace(p,x==0,0)
+			p <- replace(p,p<=0,.Machine$double.xmin)
+			p <- replace(p,p>=1,1-.Machine$double.neg.eps)
+			sum(dbinom(y,n,p,log=TRUE))			
+		}
+		lower <- c(-Inf,0)
+		upper <- c(Inf,Inf)
+		GR <- function(par,SD=SD,x,n,y){
+			newv <- rlnorm(1,par[1],SD[2])
+			newu <- rnorm(1,par[2],SD[1])
+			c(newu,newv)
+			}
+		null.LL <- LL.fn(par=c(u=0,v=1),SD,x,n,y) # straight line from (0,0) to (1,1)
+	}
+	if(model=="Beta"){
+		if(is.null(start)) start <- c(mu=0.5,nu=2)
+		SD <- rep(0.1,2)
+		k <- 2
+		LL.fn <- function(par,SD=SD,x,n,y){
+			p <- pbeta(x,max(par[1]*par[2],.Machine$double.xmin),max((1-par[1])*par[2],.Machine$double.xmin))
 			p <- replace(p,p<=0,.Machine$double.xmin)
 			p <- replace(p,p>=1,1-.Machine$double.neg.eps)
 			# dens <- dbinom(y,n,p,log=TRUE)
@@ -27,11 +74,13 @@ gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,S
 			newb <- max(.Machine$double.xmin,rnorm(1,par[2],SD[2]))
 			c(newa,newb)
 			}
-		null.LL <- LL.fn(par=c(a=.5,b=2),x,n,y) # straight line from (0,0) to (1,1)
+		null.LL <- LL.fn(par=c(mu=.5,nu=2),SD,x,n,y) # straight line from (0,0) to (1,1)
 		}
 	if(model=="Richards"){
+		if(is.null(start)) start <- c(p1=1e+5,p2=1-1e+5,b=-2*log((1e+5-1)/1e+5),m=1/2)
 		k <- 4
-		LL.fn <- function(par,x,n,y){
+		SD <- rep(0.1,4)
+		LL.fn <- function(par,SD=SD,x,n,y){
 			p <- par[1]+(par[2]-par[1])/(1+exp(par[3]*(x-par[4])))
 			p <- replace(p,p<=0,.Machine$double.xmin)
 			p <- replace(p,p>=1,1-.Machine$double.neg.eps)
@@ -47,11 +96,13 @@ gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,S
 			c(p1,p2,b,m)
 			}
 			p1 <- 1e+5
-		null.LL <- LL.fn(par=c(p1,1-p1,-2*log((p1-1)/p1),1/2),x,n,y) # a pretty straight line between (0,0) and (1,1)
+		null.LL <- LL.fn(par=c(p1,1-p1,-2*log((p1-1)/p1),1/2),SD=SD,x,n,y) # a pretty straight line between (0,0) and (1,1)
 		}
 	if(model=="Barton"){
+		if(is.null(start)) start <- c(a=0,b=0)
+		SD <- rep(0.1,2)
 		k <- 2
-		LL.fn <- function(par,x,n,y){
+		LL.fn <- function(par,SD=SD,x,n,y){
 			p <- x+2*x*(1-x)*(par[1]+par[2]*(2*x-1))
 			p <- replace(p,p<=0,.Machine$double.xmin)
 			p <- replace(p,p>=1,1-.Machine$double.neg.eps)
@@ -64,9 +115,9 @@ gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,S
 			newb <- rnorm(1,par[2],SD[2])
 			c(newa,newb)
 			}
-		null.LL <- LL.fn(par=c(a=0,b=0),x,n,y) # straight line from (0,0) to (1,1)
+		null.LL <- LL.fn(par=c(a=0,b=0),SD,x,n,y) # straight line from (0,0) to (1,1)
 		}
-	if(method!="SANN"&method!="mcmc"){
+	if(method=="L-BFGS-B"){
 		est <- optim(par=start,fn=LL.fn,x=x,n=n,y=y,method=method,lower=lower,upper=upper,control=list(fnscale=-1))
 		}
 	if(method=="SANN"){
@@ -105,6 +156,7 @@ gcline.fn <- function(x,n,y,start,model="Beta",method="L-BFGS-B",iterations=99,S
 		colnames(chain) <- names(start)
 		est <- list(par=chain[which.max(LLik),],value=max(LLik),convergence = data.frame(chain,LLik))
 		}
+	## estimation is done, now report
 	estimates <- est$par
 	convergence <- est$convergence
 	names(estimates) <- names(start)
